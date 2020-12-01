19012942460b9ec934f13ae1119f1ea3861d01b7
Ariel Xu <arielxu@live.com>
2020-12-01 11:49:30 +0800

    add controllers and routes

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,5 @@
 # app/controllers/api/v1/base_controller.rb
 class Api::V1::BaseController < ActionController::Base
-
   rescue_from StandardError,                with: :internal_server_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:create, :destroy]
+
+  ### method to save new recipe when user saves one they like
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # debugger
+    if @recipe.save
+      render json: { recipe: @recipe, status: :success }
+    else
+      render_error
+    end
+  end
+
+  ### method to see all the recipes saved
+  def my_recipes
+    @recipes = Recipe.where(user_id: params[:id])
+  end
+
+  ### method to show one singular recipe when user clicks into it based on recipe id
+  def show
+  end
+
+  ### method to scrape the recipes
+  def recipe_scraper
+  end
+
+  ### method to delete one recipe based on recipe id
+  def destroy
+    @recipe.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:user_id, :title, :ingredient, :instructions)
+  end
+
+  def render_error
+    render json: { error: @recipe.errors.full_messages }
+  end
 end
