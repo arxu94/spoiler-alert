a443f90f78e33e5135fb3020b9488ffb068cf063
Alex Tchaikovsky <xalabam94@icloud.com>
2020-12-07 16:30:56 +0800

    changed instructions column to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_091620) do
+ActiveRecord::Schema.define(version: 2020_12_07_082608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_091620) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.string "ingredient"
-    t.string "instructions"
+    t.text "instructions"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
