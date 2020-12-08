167a5726ee406bf55cf7816f656008ea43402f04
Ariel Xu <arielxu@live.com>
2020-12-08 17:53:12 +0800

    method updated to show tips for an individual user

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -35,19 +35,35 @@ class Api::V1::FoodsController < ApplicationController
 
   # this method will find most tagged categories and give suggestions
   def tips
-    @tag = ActsAsTaggableOn::Tag.most_used(1)
+    #pass in user's id
+    @user_id = params[:user_id]
+    #find all foods created by this user (how many tags in total)
+    total = Food.where(user_id: @user_id).count
+    @user = User.find(@user_id)
+    # find specific tag total (for ex how many veggies)
+    tag_hashes = @user.foods.tag_counts
+    sorted_tag_hashes = tag_hashes.sort_by { |tag| -tag.taggings_count }
+    most_popular_tag = sorted_tag_hashes[0]
+    top = most_popular_tag.taggings_count
+    percentage = ((top.to_f / total.to_f)*100).round(1)
+    p percentage
+# access the name of the tag that is most used, not the entire object of the tag
+    @tag = most_popular_tag.name
+    p @tag
+
     if @tag == "Meat and Fish"
-      message = "You have a lot of meat and fish, make sure you eat some veggies!"
+      @message = "Your fridge is #{percentage}% meat and fish, make sure you eat some veggies!"
     elsif @tag == "Dairy"
-      message = "Your fridge is % dairy products, you sure do love your cheeses!"
+      @message = "Your fridge is #{percentage}% dairy products, you sure do love your cheeses!"
     elsif @tag == "Fruits and Veggies"
-      message = "You have a lot of fruits and veggies, keep it up!"
+      @message = "Your fridge is #{percentage}% fruits and veggies, keep it up!"
     elsif @tag == "Condiments"
-      message = "You sure have a lot of sauces, make sure you put them to good use!"
+      @message = "You sure have a lot of sauces (#{percentage}% of your fridge!), make sure you put them to good use!"
     elsif @tag == "Eggs"
-      message = "You have a lot of fruits and veggies, keep it up!"
+      @message = "Your fridge is #{percentage}% eggs, keep it up!"
     end
-    @response = { most_used: @tag, message: message }
+
+    @response = { most_used: @tag, message: @message, sorted: sorted_tag_hashes }
     render json: @response
   end
 
